148ac78805e2c04644740946349453ecbcd4d3f1
Vago16 <116769696+Vago16@users.noreply.github.com>
2023-11-24 01:49:34 -0500

    Update App.test.js

--- Capstone-3/movi-app/src/App.test.js
+++ Capstone-3/movi-app/src/App.test.js
@@ -1,8 +1,74 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import renderer from 'react-test-renderer';
+import sum from './mockTestFile/sum';
+import Dashboard from './components/Dashboard';
+import AddFavourites from './components/AddFavourites';
+import Home from './components/Home';
+import MovieListHeading from './components/MovieListHeading';
+import RemoveFavourites from './components/RemoveFavourites';
+import SearchBox from './components/SearchBox';
+import Login from './containers/Login';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//makes sure jest is working
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
 });
+
+test('adds -1 + 2 to equal 1', () => {
+  expect(sum(-1, 2)).toBe(1);
+});
+
+//components render properly
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<AddFavourites />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Dashboard />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Home />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<MovieListHeading />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<RemoveFavourites />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<SearchBox />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+//container renders correctly
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Login />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+
+
+
+
